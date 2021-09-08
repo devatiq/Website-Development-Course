f1c79383094f27c9159d718d7b0bfc1f0812b73c
Atiqur Rahman <atiqsau@gmail.com>
2021-09-08 22:56:46 +0600

    Class 3 on HTML ended

---
+++ HTML/js/scripts.js
@@ -0,0 +1,6 @@
+function changtext() {
+document.getElementById('originaltxt').innerHTML = 'This is Change Text';
+document.getElementById('originaltxt').style.color = 'red';
+document.getElementById('originaltxt').style.fontSize = '30px';
+document.getElementById('originaltxt').style.backgroundColor = 'yellow';
+}
