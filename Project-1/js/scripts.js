0dc3da67078bcd7c75c12878842a6a7fbd1d950a
Atiqur Rahman <atiqsau@gmail.com>
2021-09-12 21:37:13 +0600

    Project 1 with HTML and Simple CSS

---
+++ Project-1/js/scripts.js
@@ -0,0 +1,5 @@
+function ourclick() {
+document.getElementById('ctahead').innerHTML ="This is our Change Heading";
+document.getElementById('ctahead').style.color="#d2fcfc";
+document.getElementById('ctahead').style.fontSize="50px";
+}
