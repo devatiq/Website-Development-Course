ef6a8ce1bc8853d5a7eec740ac0e823540f56dcd
devatiq <devatiqbd@gmail.com>
2021-10-06 23:42:07 +0600

    First Class On JavaScript Ended

---
+++ JS/scripts/1.js
@@ -0,0 +1,36 @@
+/*
+abstract, arguments await* boolean
+break	byte	case	catch
+char	class*	const	continue
+debugger	default	delete	do
+double	else	enum*	eval
+export*	extends*	false	final
+finally	float	for	function
+goto	if	implements	import*
+in	instanceof	int	interface
+let*	long	native	new
+null	package	private	protected
+public	return	short	static
+super*	switch	synchronized	this
+throw	throws	transient	true
+try	typeof	var	void
+volatile	while	with	yield
+*/
+
+//how write a variable
+//var a = 15; //statement 1
+//var b = 10; // statement 2
+//var c = a + b; // statement 
+
+var a = null;
+var myName = "Atiqur";
+myName = "Rahman";
+
+
+
+
+//output 
+console.log(myName);
+
+
+
