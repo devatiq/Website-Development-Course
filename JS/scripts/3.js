2ac0e22c3a0230c4fc6033d023d836a14243b77b
devatiq <devatiqbd@gmail.com>
2021-10-14 01:14:03 +0600

    Class 3 on JavaScript ended

---
+++ JS/scripts/3.js
@@ -0,0 +1,60 @@
+/*
+**JS Events
+ - onchange // An HTML element has been changed
+ - onclick // The user clicks an HTML element
+ -onmouseover // The user moves the mouse over an HTML element
+ -onmouseout // The user moves the mouse away from an HTML element
+ - onload // The browser has finished loading the page
+
+**JS Strings
+ - length //Find the length of a string
+ - Escape Character
+ - Extracting String Parts [slice(start,end), substring(start, end),substr(start, length)]
+ - replace() //replaces a specified value with another value
+ - toUpperCase() //converted to upper case with
+ - toLowerCase() //converted to lower case with
+
+**JS Numbers
+ - NaN //Not a Number
+ - toString() //returns a number as a string
+ - toExponential() // rounded and written using exponential notation
+ - toFixed()// returns a string, with the number written with a specified number of decimals
+ - toPrecision() //returns a string, with a number written with a specified length
+ - valueOf() // returns a number as a number.
+
+JS Arrays
+ - length // Returns the number of elements
+ - toString() // Converting Arrays to Strings
+ - join() // similar as toString but we can specify a separator (*)
+ - pop() //removes the last element
+ - push() // adds a new element at end
+ - shift() // removes the first array element and "shifts" all other elements to a lower index
+ - unshift() //adds a new element to an array at the beginning
+ - sort() // sorts an array alphabetically
+ - indexOf() // searches an array for a specified item and returns its position.
+*/
+
+
+const cars = ["Volvo", "BMW", "Toyota", "Porshe"];
+
+let x = cars.indexOf("toyota");
+
+console.log(x);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
